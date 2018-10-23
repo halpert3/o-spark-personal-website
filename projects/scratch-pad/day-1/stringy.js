b69b598aa652ad60a9669b5e139b27aa56c0eb51
Henry Alpert <halpert3@gmail.com>
2018-10-23 20:45:54 +0000

    repo remake

---
+++ projects/scratch-pad/day-1/stringy.js
@@ -0,0 +1,282 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: stringy.js
+ */
+
+/**
+ * Given an input String, return its length.
+ *
+ * TIP: In JavaScript, how can we decipher the length of a String?
+ * work?
+ */
+function length(string) {
+    // YOUR CODE BELOW HERE //
+
+    return string.length;
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to lowercase.
+ */
+function toLowerCase(string) {
+    // YOUR CODE BELOW HERE //
+
+    return string.toLowerCase();
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to uppercase.
+ */
+function toUpperCase(string) {
+    // YOUR CODE BELOW HERE //
+
+    return string.toUpperCase();
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String, return a new String forced to dash-case.
+ *
+ * Examples:
+ *
+ *      toDashCase('Hello World'); // => 'hello-world'
+ *
+ * TIP: How can you look for and replace a space in a String? How can you
+ *      enforce lowercase? Look at the global replace link below, or, try String
+ *      methods split and join?
+ *
+ *      See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#Using_global_and_ignore_with_replace()
+ */
+function toDashCase(string) {
+    // YOUR CODE BELOW HERE //
+
+    return string.toLowerCase().replace(/ /g, '-');
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * begins with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      beginsWith('Max', 'm'); // => true;
+ *      beginsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function beginsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+
+    var arr = string.split("");
+    var firstLetter = arr[0].toLowerCase();
+    var x = char.toLowerCase();
+    if (firstLetter === x) {
+        return true
+    }
+    else {
+        return false
+    }
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input String and a single character, return true if the String
+ * ends with the character, false otherwise. The Function is case insensitive.
+ *
+ * Example:
+ *
+ *      endsWith('Max', 'X'); // => true;
+ *      endsWith('Max', 'z'); // => false;
+ *
+ * TIP: How can you use Array access to your advantage here? How can you
+ *      ensure uppercase and lowercase can be compared equally?
+ */
+function endsWith(string, char) {
+    // YOUR CODE BELOW HERE //
+
+    var arr = string.split("");
+    var lastLetter = arr[arr.length - 1].toLowerCase();
+    var x = char.toLowerCase();
+    if (lastLetter === x) {
+        return true
+    }
+    else {
+        return false
+    }
+
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two input Strings, return the Strings concatenated into one.
+ *
+ * TIP: What's the operator to concatenate two Strings?
+ */
+function concat(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+
+        return stringOne + stringTwo;
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given any number of Strings, return all of them joined together.
+ *
+ * Example:
+ *
+ *      join("my", "name", "is", "Ben"); // => "mynameisBen";
+ *
+ * TIP: This Function pulls out all the arguments passed to it and stores them
+ *      in an Array called args.
+ */
+function join(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+    var args = Array.from(arguments);
+        var i; 
+
+     var text = args[0]
+    for (i = 1; i < args.length; i++) { 
+      
+      text += args[i];
+ };
+  return text;
+    
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return the longest of the two.
+ *
+ * Example:
+ *
+ *      longest("ben", "maggie");   //-> "maggie"
+ *
+ * TIP: What property of the String do we need to compare?
+ */
+function longest(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+
+     if (stringOne.length > stringTwo.length) {
+        return stringOne
+      } else {
+        return stringTwo
+      }
+
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given two Strings, return 1 if the first is higher in alphabetical order than
+ * the second, return -1 if the second is higher in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortAscending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+
+if (stringOne == stringTwo) {
+        return 0
+    }
+    if (stringOne.length > stringTwo.length) {
+        var wordLength = stringOne.length
+    } else {
+        var wordLength = stringTwo.length
+    };
+
+    var i;
+    for (i=0; i < wordLength; i++) {
+        if (stringOne[i] == stringTwo [i]) {
+            ;
+        }
+        else if ((stringOne[i] < stringTwo [i]) || (stringTwo[i] == undefined)) {
+            console.log(stringOne[i]);
+            return 1;
+            
+        
+        } else  {
+            console.log(stringTwo[i]);
+            return -1;
+        }
+    }
+
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+/**
+ * Given two Strings, return 1 if the first is lower in alphabetical order than
+ * the second, return -1 if the second is lower in alphabetical order than the
+ * first, and return 0 if they're equal.
+ *
+ * TIP: How can we compare Strings? Is 'a' greater than or less than 'b'?
+ */
+function sortDescending(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+
+
+    if (stringOne == stringTwo) {
+        return 0
+    }
+    if (stringOne.length > stringTwo.length) {
+        var wordLength = stringOne.length
+    } else {
+        var wordLength = stringTwo.length
+    };
+
+    var i;
+    for (i=0; i < wordLength; i++) {
+        if (stringOne[i] == stringTwo [i]) {
+            ;
+        }
+        else if ((stringOne[i] < stringTwo [i]) || (stringTwo[i] == undefined)) {
+            
+            return -1;
+            
+        
+        } else  {
+            
+            return 1;
+        }
+    }
+
+
+
+    // YOUR CODE ABOVE HERE //
+}
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if ((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.length = length;
+    module.exports.toLowerCase = toLowerCase;
+    module.exports.toUpperCase = toUpperCase;
+    module.exports.toDashCase = toDashCase;
+    module.exports.beginsWith = beginsWith;
+    module.exports.endsWith = endsWith;
+    module.exports.concat = concat;
+    module.exports.join = join;
+    module.exports.longest = longest;
+    module.exports.sortAscending = sortAscending
+    module.exports.sortDescending = sortDescending;
+}
